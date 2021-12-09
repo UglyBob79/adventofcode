333f302e8927e0924d9612a2af9f5f4b0386cfcc
Mattias Månsson <mattias.mansson@verisure.com>
2021-12-08 22:45:41 -0600

    Day 2

--- 2.py
+++ 2.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
+import numpy as np
 
 with open("2.input") as file:
     data = [row.split(' ') for row in file.read().splitlines()]
+    v_dir = {'forward' : 0, 'up' : -1, 'down' : 1}
 
     x = sum([int(b) for (a,b) in filter(lambda v: v[0] == "forward", data)])
-    y = sum([int(b) if a == "down" else -int(b) for (a,b) in filter(lambda v: v[0] == "up" or v[0] == "down", data)])
+    vert = [v_dir[a]*int(b) for (a,b) in data]
+    y = sum(vert)
+
+    print(x*y)
+
+    aim = np.cumsum(vert)
+    y = sum([int(a[1])*b for (a,b) in filter(lambda d: d[0][0] == "forward", zip(data, aim))])
 
     print(x*y)
