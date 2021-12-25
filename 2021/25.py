9fcff8f40c107cf7e96f06437acf92e78ae7ce23
Mattias Månsson <mattias.mansson@verisure.com>
2021-12-25 04:01:11 -0600

    Day 25 part 1

---
+++ 2021/25.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import copy
+
+if __name__ == '__main__':
+    with open("25.input") as file:
+        ocean = [bytearray(row.strip(), 'utf-8') for row in file.readlines()]
+
+        step = 0
+        while True:
+            moved = False
+            next = copy.deepcopy(ocean)
+            for y in range(len(ocean)):
+                for x in range(len(ocean[0])):
+                    if chr(ocean[y][x]) == '>':
+                        if chr(ocean[y][(x + 1) % len(row)]) == '.':
+                            next[y][x] = ord('.')
+                            next[y][(x + 1) % len(row)] = ord('>')
+                            moved = True
+
+            ocean = next
+            next = copy.deepcopy(ocean)
+            for x in range(len(ocean[0])):
+                for y in range(len(ocean)):
+                    if chr(ocean[y][x]) == 'v':
+                        if chr(ocean[(y + 1) % len(ocean)][x]) == '.':
+                            next[y][x] = ord('.')
+                            next[(y + 1) % len(ocean)][x] = ord('v')
+                            moved = True
+
+            ocean = next
+            step += 1
+
+            if not moved:
+                print(step)
+                break
