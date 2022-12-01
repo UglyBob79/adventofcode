8d42524cd55a1694421e082e7b285c3d38826e9b
Mattias Månsson <mattias.mansson@verisure.com>
2022-12-01 15:12:17 -0600

    And shorter...

--- 2022/1.py
+++ 2022/1.py
@@ -5,8 +5,7 @@ import itertools
 with open("1.input") as file:
     data = [int(x.strip()) if x.strip() else None for x in file.readlines()]
 
-    elfs = [sum(list(y)) for x, y in itertools.groupby(data, lambda z: z == None) if not x]
-    elfs.sort()
+    elfs = sorted([sum(list(y)) for x, y in itertools.groupby(data, lambda z: z == None) if not x])
 
     print(elfs[-1])
     print(sum(elfs[-3:]))
