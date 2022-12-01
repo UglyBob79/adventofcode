2ab43dfc904da08a942b8dff49ef3cb8d55cbb21
Mattias Månsson <mattias.mansson@verisure.com>
2022-12-01 10:23:09 -0600

    AoC 2022 Day 1

---
+++ 2022/1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import bisect
+
+with open("1.input") as file:
+    data = [x.strip() for x in file.readlines()]
+
+    max_val = []
+    curr = 0
+
+    for val in data:
+        if not val:
+            bisect.insort(max_val, curr)
+            curr = 0
+        else:
+            curr += int(val)
+
+    print(max(max_val))
+    print(sum(max_val[-3:]))
