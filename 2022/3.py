ad8fcc5ad0daab8a3477283e3998c74b89d57fec
Mattias Månsson <mattias.mansson@verisure.com>
2022-12-03 14:55:52 -0600

    Day 3 v3

--- 2022/3.py
+++ 2022/3.py
@@ -3,8 +3,8 @@
 with open("3.input") as file:
     data = file.read().splitlines()
 
-    score = sum([sum(map(lambda a: ord(a) + (-96 if ord(a) > 90 else -38), b)) for b in [set.intersection(set(l[:int(len(l)/2)]), set(l[int(len(l)/2):])) for l in data]])
+    score = sum([sum(map(lambda a: ord(a) + (-96 if a.islower() else -38), b)) for b in [set.intersection(set(l[:int(len(l)/2)]), set(l[int(len(l)/2):])) for l in data]])
     print(score)
 
-    score = sum([sum(map(lambda a: ord(a) + (-96 if ord(a) > 90 else -38), b)) for b in [set.intersection(*[set(y) for y in x]) for x in zip(*(iter(data),) * 3)]])
+    score = sum([sum(map(lambda a: ord(a) + (-96 if a.islower() else -38), b)) for b in [set.intersection(*[set(y) for y in x]) for x in zip(*(iter(data),) * 3)]])
     print(score)
