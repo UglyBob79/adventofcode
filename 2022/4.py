5c55e0bf073fb241c232864241eac5219d80795d
Mattias Månsson <mattias.mansson@verisure.com>
2022-12-04 01:12:19 -0600

    Day 4 small fix

--- 2022/4.py
+++ 2022/4.py
@@ -6,5 +6,5 @@ with open("4.input") as file:
     match = [[a, b] for a, b in data if (a[0] >= b[0] and a[1] <= b[1] or b[0] >= a[0] and b[1] <= a[1])]
     print(len(match))
 
-    match = [(a,b) for a, b in data if set(range(a[0], a[1] + 1)).intersection(set(range(b[0], b[1] + 1)))]
+    match = [[a, b] for a, b in data if set(range(a[0], a[1] + 1)).intersection(set(range(b[0], b[1] + 1)))]
     print(len(match))
