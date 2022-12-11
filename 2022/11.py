3571b6a5b4564b7300fb7d34539dd70f9b01bc02
Mattias Månsson <mattias.mansson@verisure.com>
2022-12-11 13:28:50 -0600

    Day 11 v2

--- 2022/11.py
+++ 2022/11.py
@@ -2,12 +2,26 @@
 import operator
 import math
 import copy
+import re
+
+MONKEY_RE = re.compile('^Monkey (?P<id>\d):\n.*: (?P<items>[^\n]+)\n.*old (?P<op>.) (?P<factor>[^\n]+)\n.*by (?P<div>\d+)\n.*monkey (?P<true>\d+)\n.*monkey (?P<false>\d+)')
+
+def parse_monkey(blob):
+    m = re.match(MONKEY_RE, blob)
+    if m:
+        return {
+            'id': int(m.group('id')),
+            'inspected': 0,
+            'items': [int(i) for i in m.group('items').split(', ')],
+            'op': ((operator.pow if m.group('factor') == 'old' else operator.mul) if m.group('op') == '*' else operator.add, 2 if m.group('factor') == 'old' else int(m.group('factor'))),
+            'test': {'factor': int(m.group('div')), True: int(m.group('true')), False: int(m.group('false'))}
+        }
 
 def simulate(monkeys, rounds, divide=True):
-    prod = math.prod([monkeys[id]['test']['factor'] for id in monkeys])
+    prod = math.prod([monkey['test']['factor'] for monkey in monkeys])
 
-    for _round in range(rounds):
-        for monkey in monkeys.values():
+    for _ in range(rounds):
+        for monkey in monkeys:
             while len(monkey['items']) > 0:
                 item = monkey['items'].pop(0)
                 monkey['inspected'] += 1
@@ -15,30 +29,13 @@ def simulate(monkeys, rounds, divide=True):
                 item = item // 3 if divide else item % prod
                 monkeys[monkey['test'][item % monkey['test']['factor'] == 0]]['items'].append(item)
 
-    return math.prod(sorted([monkeys[id]['inspected'] for id in monkeys])[-2:])
+    return math.prod(sorted([monkey['inspected'] for monkey in monkeys])[-2:])
 
 with open("11.input") as file:
-    monkeys = {}
-    monkey = None
+    monkeys = []
 
-    for line in [l.strip() for l in file.read().splitlines()]:
-        if line.startswith('Monkey'):
-            monkey = {'id': int(line.split(' ')[1][0]), 'inspected': 0}
-        elif line.startswith('Starting items:'):
-            monkey['items'] = [int(i) for i in line[16:].split(', ')]
-        elif line.startswith('Operation:'):
-            parts = line.split(' ')
-            if parts[4] == '*':
-                monkey['op'] = (operator.pow, 2) if parts[5] == 'old' else (operator.mul, int(parts[5]))
-            elif parts[4] == '+':
-                monkey['op'] = (operator.add, int(parts[5]))
-        elif line.startswith('Test:'):
-            monkey['test'] = {'factor': int(line.split(' ')[3])}
-        elif line.startswith('If true:'):
-            monkey['test'][True] = int(line.split(' ')[5])
-        elif line.startswith('If false:'):
-            monkey['test'][False] = int(line.split(' ')[5])
-            monkeys[monkey['id']] = monkey
+    for blob in file.read().split('\n\n'):
+        monkeys.append(parse_monkey(blob))
 
     print(simulate(copy.deepcopy(monkeys), 20))
     print(simulate(copy.deepcopy(monkeys), 10000, False))
