224f3b041d636e0800de6a9116b4cd65d43edd69
Mattias Månsson <mattias.mansson@verisure.com>
2023-12-03 18:23:17 -0600

    Day 3 minor cleanup

--- 2023/3.dart
+++ 2023/3.dart
@@ -10,12 +10,13 @@ void scanScematic(List<String> data) {
     for (RegExpMatch match in RegExp(r'\d+').allMatches(row)) {
       for (int sy = y - 1; sy <= y + 1; sy++) {
         for (int sx = match.start - 1; sx <= match.end; sx ++) {
-          if (sx > 0 && sy > 0 && sx < row.length && sy < data.length) {
-            if (data[sy].codeUnitAt(sx) != 46 && (data[sy].codeUnitAt(sx) < 48 || data[sy].codeUnitAt(sx) > 57)) {
-              parts[[match.start, y].toString()] = int.parse(match.group(0)!);
-              if (data[sy][sx] == '*')
-                gears.putIfAbsent([sx, sy].toString(), () => []).add(int.parse(match.group(0)!));
-            }
+          if (sx < 0 || sy < 0 || sx >= row.length || sy >= data.length) continue;
+
+          if (data[sy].codeUnitAt(sx) != 46 && (data[sy].codeUnitAt(sx) < 48 || data[sy].codeUnitAt(sx) > 57)) {
+            parts[[match.start, y].toString()] = int.parse(match.group(0)!);
+
+            if (data[sy][sx] == '*')
+              gears.putIfAbsent([sx, sy].toString(), () => []).add(int.parse(match.group(0)!));
           }
         }
       }
