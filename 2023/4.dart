4cb07b89e9f82d6b5b8f265938f6733ca54334c0
Mattias Månsson <mattias.mansson@verisure.com>
2023-12-04 10:10:09 -0600

    Day 4 dirty score

--- 2023/4.dart
+++ 2023/4.dart
@@ -2,23 +2,20 @@
 
 import 'dart:io';
 
-void part1(List<List<List<int>>> cards) {
-  var sum = cards
-      .map((element) =>
-          element[0].where((item) => element[1].contains(item)).toList().length)
+void part1(List<int> score) {
+  var sum = score
       .map((element) => element == 0 ? 0 : 1 << (element - 1))
       .reduce((value, element) => value + element);
 
   print(sum);
 }
 
-void part2(List<List<List<int>>> cards) {
-  List<int> instances = List<int>.filled(cards.length, 1);
+void part2(List<int> score) {
+  List<int> instances = List<int>.filled(score.length, 1);
 
-  for (int i = 0; i < cards.length; i++) {
+  for (int i = 0; i < score.length; i++) {
     if (instances[i] > 0) {
-      int count = cards[i][0].where((item) => cards[i][1].contains(item)).length;
-      for (int j = i + 1; j <= i + count; j++) {
+      for (int j = i + 1; j <= i + score[i]; j++) {
         instances[j] += instances[i];
       }
     }
@@ -30,14 +27,8 @@ void part2(List<List<List<int>>> cards) {
 void main() {
   List<String> data = new File("4.input").readAsLinesSync().toList();
 
-  List<List<List<int>>> cards = data.map((card) {
-    var match = RegExp(r'Card\s+([0-9]+):([^|]+)\|(.*)').firstMatch(card);
-    var winning = match!.group(2)!.trim().split(RegExp(r'\s+')).map((e) => int.parse(e)).toList();
-    var numbers = match.group(3)!.trim().split(RegExp(r'\s+')).map((e) => int.parse(e)).toList();
+  var score = data.map((card) => RegExp(r'\d+(?![:\d])').allMatches(card).map((match) => match.group(0)!).toSet()).map((set) => 35 - set.length).toList();
 
-    return [winning, numbers];
-  }).toList();
-
-  part1(cards);
-  part2(cards);
+  part1(score);
+  part2(score);
 }
