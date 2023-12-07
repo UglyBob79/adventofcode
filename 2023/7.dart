3e4dc16470634a25110e5b301c31200ced954ec3
Mattias Månsson <mattias.mansson@verisure.com>
2023-12-07 16:05:22 -0600

    Day 7 optimized

--- 2023/7.dart
+++ 2023/7.dart
@@ -40,14 +40,39 @@ List<int> handCount(String hand, List<int> handVals) {
 }
 
 String handSignature(String hand, List<int> handVals) {
-  List<int> count = handCount(hand, handVals).where((element) => element != 0).toList();
-  count.sort((a, b) => b.compareTo(a));
-
+  List<int> count = handCount(hand, handVals).where((element) => element != 0).toList()..sort((a, b) => b.compareTo(a));
   return count.join('');
 }
 
-int calcScore(int handType, String hand) {
-  int handScore = handType;
+int calcHandType(String hand) {
+  String handSig =
+      (handCount(hand, hand.split('').map((c) => cardMap[c]!).toList())
+              .where((element) => element != 0)
+              .toList()
+            ..sort((a, b) => b.compareTo(a)))
+          .join();
+
+  return handType[handSig]!;
+}
+
+int calcScore(int handType, String hand, { bool jokers = false }) {
+  String newHand = hand;
+
+  if (jokers) {
+    List<int> handVals = newHand.split('').map((c) => cardMap[c]!).toList();
+    List<int> count = handCount(newHand, handVals);
+
+    int jokers = count[cardMap['J']!];
+
+    if (jokers > 0) {
+      count[cardMap['J']!] = 0;
+      int highest = count.lastIndexOf(count.reduce((value, element) => value > element ? value : element));
+
+      newHand = newHand.replaceAll('J', reverseCardMap[highest]!);
+    }
+  }
+
+  int handScore = calcHandType(newHand);
 
   hand.split('').map((c) => cardMap[c]!).forEach((c) {
     handScore <<= 8;
@@ -57,63 +82,15 @@ int calcScore(int handType, String hand) {
   return handScore;
 }
 
-int calcHandType(String hand) {
-  List<int> handVals = hand.split('').map((c) => cardMap[c]!).toList();
-  return handType[handSignature(hand, handVals)]!;
-}
-
-String replaceJokers(String hand, String card) {
-  return hand.replaceAll('J', card);
-}
-
-int maxHandScore(int handType, String hand) {
-  String newHand = hand;
-  List<int> handVals = newHand.split('').map((c) => cardMap[c]!).toList();
-  List<int> count = handCount(newHand, handVals);
-
-  if (count[cardMap['J']!] > 0) {
-    String handSig = handSignature(newHand, handVals);
-
-    if (handSig == '5') {
-      newHand = 'AAAAA';
-    } else if (handSig == '41' || handSig == '32' || handSig == '11111') {
-      newHand = replaceJokers(newHand, reverseCardMap[count.lastIndexWhere((element) => element != 0)]!);
-    } else if (handSig == '311') {
-      if (count.indexOf(3) == cardMap['J']) {
-        newHand = replaceJokers(newHand, reverseCardMap[count.lastIndexWhere((element) => element != 0)]!);
-      } else {
-        newHand = replaceJokers(newHand, reverseCardMap[count.indexWhere((element) => element == 3)]!);
-      }
-    } else if (handSig == '221') {
-      if (count.indexOf(1) == cardMap['J']) {
-        newHand = replaceJokers(newHand, reverseCardMap[count.lastIndexWhere((element) => element != 0)]!);
-      } else {
-        newHand = replaceJokers(newHand, reverseCardMap[count.lastIndexWhere((element) => element == 2)]!);
-      }
-    } else if (handSig == '2111') {
-      if (count.indexOf(1) == cardMap['J']) {
-        newHand = replaceJokers(newHand, reverseCardMap[count.lastIndexWhere((element) => element == 2)]!);
-      } else {
-        newHand = replaceJokers(newHand, reverseCardMap[count.lastIndexWhere((element) => element != 0)]!);
-      }
-    }
-  }
-
-  return calcScore(calcHandType(newHand), hand);
-}
-
 void part(int part, List<Map<String, Object>> hands) {
   if (part == 2)
     cardMap['J'] = 1;
 
-  hands.forEach((hand) {
-    hand['score'] = (part == 1)
-        ? calcScore(
-            calcHandType((hand['hand'] as String)), (hand['hand'] as String))
-        : maxHandScore(
-            calcHandType((hand['hand'] as String)), (hand['hand'] as String));
-  });
-  hands.sort((a, b) => (a['score']! as int).compareTo((b['score']! as int)));
+  hands..forEach((hand) {
+      hand['score'] = calcScore(
+          calcHandType((hand['hand'] as String)), (hand['hand'] as String),
+          jokers: part == 2);
+    })..sort((a, b) => (a['score']! as int).compareTo((b['score']! as int)));
 
   int i = 1;
 
@@ -122,17 +99,10 @@ void part(int part, List<Map<String, Object>> hands) {
 }
 
 void main() {
-  List<String> data = new File("7.input")
+  List<Map<String, Object>> hands = new File("7.input")
       .readAsLinesSync()
-      .toList();
-
-  List<Map<String, Object>> hands = data
       .map((row) => row.split(' '))
-      .map((hand) => {
-            'hand': hand[0],
-            'bid': int.parse(hand[1]),
-            'score': 0
-          })
+      .map((hand) => {'hand': hand[0], 'bid': int.parse(hand[1]), 'score': 0})
       .toList();
 
   part(1, hands);
