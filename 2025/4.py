04adde47f06724858210d0329ecbe637ce0c4c21
Mattias Månsson <mattias.mansson@verisure.com>
2025-12-04 18:55:27 -0600

    2025 Day 4 new version

--- 2025/4.py
+++ 2025/4.py
@@ -11,26 +11,22 @@ def neighbours(coords, x, y):
 
 def count(coords, remove=False):
     c = 0
-
-    while True:
-        curr = c
-        for y in range(len(grid)):
-            for x in range(len(grid[0])):
-                if (x, y) in coords and neighbours(coords, x, y) < 4:
-                    c += 1
-                    if remove:
-                        coords.remove((x, y))
-        if not remove or c == curr:
-            break
     
+    while True:
+        to_remove = {(x, y) for (x, y) in coords if neighbours(coords, x, y) < 4}
+        c += len(to_remove)
+
+        if remove:
+            coords -= to_remove
+
+        if not remove or len(to_remove) == 0:
+            break
+
     return c
 
 with open("4.input") as file:
     grid = [list(line.strip()) for line in file.readlines()]
-    coords = {(x, y)
-                for y, row in enumerate(grid)
-                for x, char in enumerate(row)
-                if char == '@'}
+    coords = {(x, y) for y, row in enumerate(grid) for x, char in enumerate(row) if char == '@'}
 
     print(count(coords))
     print(count(coords, remove=True))
